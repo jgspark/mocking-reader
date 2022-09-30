e314547f02b07f905afffda79c2468a4f6163b32
newbalancer <pnw123456@icloud.com>
2022-09-30 23:59:00 +0900

    feat: method add

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,9 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework:spring-core:5.3.22")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.0.1")
+    implementation("org.mockito:mockito-core:2.1.0")
     testImplementation(kotlin("test"))
 }
 
