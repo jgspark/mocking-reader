8938cd2bf12b2861ff07bdac293a45ea097adf53
spark <pnw123456@icloud.com>
2023-04-17 21:47:29 +0900

    feat: java8 version update

--- src/main/java/org/jgspark/reader/impl/SpringReader.java
+++ src/main/java/org/jgspark/reader/impl/SpringReader.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class SpringReader implements Reader {
-    
+
     private final String path;
 
     public SpringReader(String path) {
