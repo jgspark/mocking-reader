bcef5672f415e10425980875ba6cc77d1d8fa3c6
spark <pnw123456@icloud.com>
2023-04-21 23:21:51 +0900

    feat: reader static method add

--- src/main/java/org/jgspark/Main.java
+++
@@ -1,7 +0,0 @@
-package org.jgspark;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/main/java/org/jgspark/reader/Reader.java
+++ src/main/java/org/jgspark/reader/Reader.java
@@ -1,6 +1,42 @@
 package org.jgspark.reader;
 
-public interface Reader {
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.ClassPathResource;
 
-    <T> T readJson(Class<T> type);
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Reader {
+
+    public static <T> T readJson(String path, Class<T> type) {
+
+        File jsonFile = null;
+
+        try {
+            jsonFile = new ClassPathResource(path).getFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String fileString = null;
+
+        try {
+            fileString = new String(Files.readAllBytes(jsonFile.toPath()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+
+        try {
+            return objectMapper.readValue(fileString, type);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
